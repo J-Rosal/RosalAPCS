2b7434ff544878323a436f3ce20d39f73368a8dd
John Arthur Rosal <Rosal.John2019@ssusd.org>
2018-02-14 07:18:57 -0800

    practice

---
+++ Functions/functions.java
@@ -0,0 +1,19 @@
+class ValidationUtils {
+    public static Boolean isValidString(Scanner scan) {
+        try {
+            String s = scan.nextLine();
+        }
+        return 
+            s.toLowerCase.equals("T")
+            s.toLowerCase.equals("F")
+            s.toLowerCase.equals("T")
+            s.toLowerCase.equals("F")
+    }
+}
+
+
+
+
+
+        
+   
