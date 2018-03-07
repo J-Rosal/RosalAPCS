677bc7f8ad858e1b6940c95f1a3890fd74acb32a
John Arthur Rosal <Rosal.John2019@ssusd.org>
2018-03-07 08:02:25 -0800

    Loops Practice

---
+++ Assignments/Loops/Loops.java
@@ -0,0 +1,98 @@
+class Loops {
+    public static void main(String args[]) {
+            // original 
+    //           int n = 1;
+    // while(n < 10) {
+    //     System.out.println(n);
+    // }
+            //  fixed code
+    //          int n = 1;           
+    // while(n < 10) {
+    //     System.out.println(n);
+    //     n++;
+            // original 
+    //      Boolean shouldLoop = true;
+    // int n = 1;
+    // while(shouldLoop) {
+    //     if(n == 100) {
+    //         System.out.println("100 loop passes occured.");
+    //     }
+    //     n++;
+            // fixed
+    //      Boolean shouldLoop = true;
+    // int n = 1;
+    // while(shouldLoop) {
+    //     if(n == 100) {
+    //         shouldLoop = false;
+    //         System.out.println("100 loop passes occured.");
+    //     }
+    //     n++;
+            // descending number from 100 to -100
+    //      int n = 100;
+    // while(n >= -100) {
+    //     System.out.println(n);
+    //     n --;
+            // multiples of 3 up to 120
+    //      int n = 0;
+    // while(n <= 120) {
+    //     System.out.println(n);
+    //     n += 3;
+    char alpha = 'a';
+    System.out.println(alpha); 
+    alpha++;
+    System.out.println(alpha); 
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    alpha++;
+    System.out.println(alpha);
+    
+    
+        }
+    }
+ 
+
+   
