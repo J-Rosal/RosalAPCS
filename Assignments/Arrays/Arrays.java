62c49565c2ff18b7885b4bb72f4af12b74d3b64c
John Arthur Rosal <Rosal.John2019@ssusd.org>
2018-03-09 07:34:15 -0800

    Arrays Exercise

---
+++ Assignments/Arrays/Arrays.java
@@ -0,0 +1,28 @@
+class Arrays {
+    public static void main(string args[]) {
+           int[] numArray = {20, 3, 6, 10, 13, 25, 100, 6, 3, 20, 5102};
+
+    System.out.println( numArray[3] ); //printed value is 6
+
+    System.out.println( numArray[11] ); //printed value is 5102
+
+    System.out.println( numArray[1] ); //printed value is 20
+
+    System.out.println( numArray[7] ); //printed value is 100
+
+    System.out.println( numArray[8] ); //printed value is 6
+    }
+}
+
+    //2.
+    int[] numArray = {20, 3, 6, 10, 13, 25, 100, 6, 3, 20, 5102};
+
+        System.out.println( numArray[5] );
+
+        System.out.println( numArray[2] );
+
+        System.out.println( numArray[11] );
+
+        System.out.println( numArray[6] );
+
+        System.out.println( numArray[1] );
