d033fc8276caec78a8215313dc213b5c765c1626
John Arthur Rosal <Rosal.John2019@ssusd.org>
2018-03-09 07:38:23 -0800

    Arrays Exercise

--- Assignments/Arrays/Arrays.java
+++ Assignments/Arrays/Arrays.java
@@ -1,5 +1,6 @@
 class Arrays {
     public static void main(string args[]) {
+    //1.       
            int[] numArray = {20, 3, 6, 10, 13, 25, 100, 6, 3, 20, 5102};
 
     System.out.println( numArray[3] ); //printed value is 6
@@ -11,11 +12,9 @@ class Arrays {
     System.out.println( numArray[7] ); //printed value is 100
 
     System.out.println( numArray[8] ); //printed value is 6
-    }
-}
 
     //2.
-    int[] numArray = {20, 3, 6, 10, 13, 25, 100, 6, 3, 20, 5102};
+            int[] numArray = {20, 3, 6, 10, 13, 25, 100, 6, 3, 20, 5102};
 
         System.out.println( numArray[5] );
 
@@ -26,3 +25,10 @@ class Arrays {
         System.out.println( numArray[6] );
 
         System.out.println( numArray[1] );
+
+    //3.
+            int[] fibonacci = {1, 1, 2, 3, 0, 8, 13, 0, 34, 0};
+
+    }
+}
+
