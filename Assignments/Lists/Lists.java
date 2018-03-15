8c8ab6d99912bf3cc0addce721083f41a0279ace
John Arthur Rosal <Rosal.John2019@ssusd.org>
2018-03-15 07:37:53 -0700

    Lists Exercise

---
+++ Assignments/Lists/Lists.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Lists {
+    public static void main(String[] args) {
+    //      ArrayList<Integer> numList = new ArrayList<Integer>();
+    // numList.add(5); 
+    // numList.add(0,10); 
+    // numList.add(1,15); 
+    // numList.remove(1); 
+    // numList.add(20);  
+    // numList.add(1,25);  
+    // numList.add(0, 30);  
+    // numList.add(3, 35);  
+    // numList.remove(2); 
+      ArrayList<Integer> numList = new ArrayList<Integer>();
+
+    numList.add(1);
+    numLists.add(2);
+    numLists.add(3);
+    numLists.add(4);
+    numLists.add(5);
+
+    for(int i = 0; i < numList.size(); i++) {
+        System.out.println(numList.get(i));
+        }
+    }
+}
+//
